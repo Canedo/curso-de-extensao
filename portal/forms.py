de7430720c7c59c0b0ab578b93240c156d191ae9
Igor Canedo <igor.canedo@corp.globo.com>
2014-11-05 22:17:46 -0200

    ajuste esqueci minha senha

--- portal/forms.py
+++ portal/forms.py
@@ -3,9 +3,11 @@ from django import forms
 from pessoa.models import Pessoa
 from django.contrib.auth.forms import PasswordResetForm
 
+
 class CustomPasswordResetForm(PasswordResetForm):
     def clean_email(self):
         email = self.cleaned_data['email']
-        if not Pessoa.objects.filter(email=email).exists():
+
+        if not Pessoa.objects.filter(contato__descricao=email).exists():
             raise forms.ValidationError('Por favor, insira um email válido cadastrado no sistema.')
         return email
--- portal/views.py
+++ portal/views.py
@@ -11,8 +11,8 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from disciplina.models import Avaliacao
 from pessoa.models import Pessoa
-from django.contrib.sites.shortcuts import get_current_site
 from portal.forms import CustomPasswordResetForm
+
 
 def home(request):
     return render(request, 'base.html')
@@ -74,6 +74,7 @@ def alterar_senha(request):
 
     return render(request, 'change_password.html', {'form': form})
 
+
 def esqueci_senha(request):
     return password_reset(request,
                           template_name='forgot_password.html',
@@ -81,10 +82,13 @@ def esqueci_senha(request):
                           subject_template_name='email/password_reset_confirm_subject.txt',
                           html_email_template_name='email/password_reset_confirm',)
 
+
 def esqueci_senha_sucesso(request):
     messages.add_message(request, messages.SUCCESS, 'Um email será enviado em breve para o endereço de email informado com mais informações para redefinir a sua senha.')
     return HttpResponseRedirect(reverse('home'))
 
+
 def redefinir_senha(request, uidb64=None, token=None):
-    return password_reset_confirm(request, template_name='password_reset.html',
+    return password_reset_confirm(
+        request, template_name='password_reset.html',
         uidb64=uidb64, token=token, post_reset_redirect=reverse('home'))
