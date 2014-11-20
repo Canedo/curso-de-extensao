20f030c9b7e129ea043a5672a8826e43e04fefc2
Igor Canedo <igor.canedo@corp.globo.com>
2014-11-20 15:22:33 -0200

    ajustando admin de downloads

--- downloads/static/js/cursoextensao-historico_downloads.js
+++ downloads/static/js/cursoextensao-historico_downloads.js
@@ -6,19 +6,31 @@ adminHistoricoDownload = (function () {
     var resultList = document.getElementById('result_list'),
         resultListBody = resultList.getElementsByTagName('tbody')[0],
         lengthOfResultList = resultListBody.getElementsByTagName('tr').length,
-        paginator = document.getElementsByClassName('paginator')[0];
+        paginator = document.getElementsByClassName('paginator')[0],
+        contentContainer = document.getElementById('content'),
+        headerContent = contentContainer.getElementsByTagName('h1')[0],
+        stringToMatch = 'Estatísticas';
 
     function init() {
+        headerDownloads();
         mensagemHistoricoDownload();
     }
 
     function mensagemHistoricoDownload() {
-        if (lengthOfResultList == 1) {
-            paginator.textContent = lengthOfResultList + ' download realizado';
-        } else if (lengthOfResultList == 0) {
-            paginator.textContent = 'Nenhum download realizado';
-        } else {
-            paginator.textContent = lengthOfResultList + ' downloads realizados';
+        if (headerContent.textContent.indexOf(stringToMatch) > -1) {
+            if (lengthOfResultList == 1) {
+                paginator.textContent = lengthOfResultList + ' download realizado';
+            } else if (lengthOfResultList == 0) {
+                paginator.textContent = 'Nenhum download realizado';
+            } else {
+                paginator.textContent = lengthOfResultList + ' downloads realizados';
+            }
+        }
+    }
+
+    function headerDownloads() {
+        if (headerContent.textContent == 'Selecione Estatísticas de Download para modificar'){
+            headerContent.textContent = 'Estatísticas de Download';
         }
     }
 
